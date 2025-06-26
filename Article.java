f909267d365d854b3cf2ff7909989abf8e0a3437
Felix Bächle <felix.baechle@htwg-konstanz.de>
2025-06-26 13:44:35 +0200

    little change test

---
+++ Article.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author felixbaechle
+ */
+public class Article {
+    private String name;
+    private double price;
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
